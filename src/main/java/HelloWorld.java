a6ddb908c2c42a6c9c91a14063700d3eed78c59b
darcipeoples <darcipeoples@wirelessprv-10-195-116-149.near.illinois.edu>
2017-09-05 20:56:01 -0500

    Changed HelloWorld message

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -14,6 +14,6 @@ public class HelloWorld {
      * @param unused unused input parameters.
      */
     public static void main(final String[] unused) {
-        Zen.drawText("Ayy lmao", 100, 200);
+        Zen.drawText("Ayy lmaooo", 100, 200);
     }
 }
